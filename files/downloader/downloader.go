35201015965bdc1b6151bbb21a5fb9622c4dae45
Loren Yeung <loreny@loreny-mac.local>
2019-08-10 17:09:17 -0700

    added sorting of timestamps, and added hashmap for data structure

--- files/downloader/downloader.go
+++ files/downloader/downloader.go
@@ -8,7 +8,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sort"
 	"strconv"
+	"sync"
+	"time"
 )
 
 // StorageJSON api/storage call
@@ -21,10 +24,24 @@ type StorageJSON struct {
 
 //FileStorageJSON API call
 type FileStorageJSON struct {
-	LastModified string `json:"lastModified"`
-	Path         string `json:"path"`
-	Size         string `json:"size"`
-	DownloadURI  string `json:"downloadUri"`
+	LastModified  string `json:"lastModified"`
+	ConvertedTime time.Time
+	Size          string `json:"size"`
+	DownloadURI   string `json:"downloadUri"`
+}
+
+type timeSlice []FileStorageJSON
+
+func (p timeSlice) Len() int {
+	return len(p)
+}
+
+func (p timeSlice) Less(i, j int) bool {
+	return p[i].ConvertedTime.Before(p[j].ConvertedTime)
+}
+
+func (p timeSlice) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
 }
 
 func main() {
@@ -36,6 +53,7 @@ func main() {
 	var result map[string]interface{}
 	var url, username, apiKey, dlLocation, repo string
 	file, err := os.Open("download.json")
+
 	if err != nil {
 		log.Fatal("error:", err)
 	} else {
@@ -57,23 +75,39 @@ func main() {
 			// fmt.Print("test new func:")
 			// fmt.Println(string(GetRestAPI(url+"/"+repo+"/"+download+"/", username, apiKey)))
 
+			var unsorted = make(map[int]FileStorageJSON)
+
 			var result2 StorageJSON
 			var data = GetRestAPI(url+"/api/storage/"+repo+"/"+download+"/", username, apiKey)
 			json.Unmarshal([]byte(data), &result2)
 			fmt.Println("Found the following files under" + url + "/" + repo + "/" + download + "/\nNumber\tLast Modified\tSize\tPath")
+			var wg sync.WaitGroup //multi threading fun
+			wg.Add(len(result2.Children))
 			for i := 0; i < len(result2.Children); i++ {
-				//need to deal with index out of range
-				//fmt.Println("data:" + result2.Children[i].URI)
-				var result3 FileStorageJSON
-				var data2 = GetRestAPI(url+"/api/storage/"+repo+"/"+download+result2.Children[i].URI, username, apiKey)
-				json.Unmarshal([]byte(data2), &result3)
+				go func(i int) {
+					defer wg.Done()
+					var result3 FileStorageJSON
+					var data2 = GetRestAPI(url+"/api/storage/"+repo+"/"+download+result2.Children[i].URI, username, apiKey)
+					json.Unmarshal([]byte(data2), &result3)
+					time, _ := time.Parse(time.RFC3339, result3.LastModified)
+					unsorted[i] = FileStorageJSON{result3.LastModified, time, result3.Size, result3.DownloadURI}
+				}(i)
+			}
+			wg.Wait()
 
-				size, err := strconv.ParseInt(result3.Size, 10, 64)
+			sorted := make(timeSlice, 0, len(unsorted))
+
+			for _, d := range unsorted {
+				sorted = append(sorted, d)
+			}
+			sort.Sort(sorted)
+			for key, value := range sorted {
+				size, err := strconv.ParseInt(value.Size, 10, 64)
 				if err != nil {
 					fmt.Printf("%d is not of type %T", size, size)
 					os.Exit(127)
 				}
-				fmt.Printf("%d\t%s\t%s\t%s\n", i+1, result3.LastModified, ByteCountDecimal(size), result3.Path)
+				fmt.Printf("%d\t%s\t%s\t%s\n", key, value.ConvertedTime, ByteCountDecimal(size), value.DownloadURI)
 			}
 		}
 	}
@@ -90,7 +124,7 @@ func ByteCountDecimal(b int64) string {
 		div *= unit
 		exp++
 	}
-	return fmt.Sprintf("%.1f %cB", float64(b)/float64(div), "kMGTPE"[exp])
+	return fmt.Sprintf("%.1f%cB", float64(b)/float64(div), "kMGTPE"[exp])
 }
 
 //GetRestAPI blah
