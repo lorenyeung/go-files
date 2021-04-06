ac6c187a800ff7740d414810a0dbbbb9e9d600b4
Loren Yeung <loren.w.yeung@gmail.com>
2021-04-05 19:05:39 -0700

    minor adjustments

--- release.sh
+++ release.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 GIT_REPO=$(jq -r '.git_repo' metadata.json)
-BINARY=$(jq -r '.binary_prefix' metadata.json)
-LATEST_SCRIPT_TAG=$(curl -s https://api.github.com/repos/lorenyeung/$GIT_REPO/releases/latest | jq -r '.tag_name')
+BINARY_PREFIX=$(jq -r '.binary_prefix' metadata.json)
+LATEST_SCRIPT_TAG=$(curl -s https://api.github.com/repos/lorenyeung/${GIT_REPO}/releases/latest | jq -r '.tag_name')
+test=$(curl -s https://api.github.com/repos/lorenyeung/${GIT_REPO}/releases/latest)
+echo "test var $test"
 LOCAL_TAG_NAME=$(jq -r '.script_version' metadata.json)
 LOCAL_NAME=$(echo "${LOCAL_TAG_NAME/v/}")
+RELEASE_BRANCH=$(jq -r '.release_branch' metadata.json)
 
 if [ "$LATEST_SCRIPT_TAG" = "$LOCAL_TAG_NAME" ]; then
     echo "Did you forget to increment metadata.json? The latest release on github ($LATEST_SCRIPT_TAG) is the same as metadata.json's ($LOCAL_TAG_NAME)"
@@ -28,7 +31,7 @@ echo "Enter body description"
 read message
 body="{
   \"tag_name\": \"$LOCAL_TAG_NAME\",
-  \"target_commitish\": \"main\",
+  \"target_commitish\": \"$RELEASE_BRANCH\",
   \"name\": \"$LOCAL_NAME\",
   \"body\": \"$message\",
   \"draft\": false,
@@ -51,9 +54,9 @@ echo "Looks good?"
         case $yn in
             Yes)
                 echo $body > release.json 
-                curl -u $GIT_USER:$GIT_TOKEN -XPOST https://api.github.com/repos/lorenyeung/$GIT_REPO/releases -H "Content-Type: application/json" -T release.json -o release-response.json
+                curl -u $GIT_USER:$GIT_TOKEN -XPOST "https://api.github.com/repos/lorenyeung/$GIT_REPO/releases" -H "Content-Type: application/json" -T release.json -o release-response.json
                 rm release.json
-                BINARIES=("$BINARY-darwin-x64" "$BINARY-linux-x64")
+                BINARIES=("$BINARY_PREFIX-darwin-x64" "$BINARY_PREFIX-linux-x64")
                 ASSET_URL=$(jq -r '.upload_url' release-response.json)
                 edited=$(echo $ASSET_URL | sed 's/{?name,label}//')
 
@@ -63,7 +66,7 @@ echo "Looks good?"
                     curl -T $BINARY "$URL" -H "Content-type: application/x-binary" -u $GIT_USER:$GIT_TOKEN
                 done
                 rm release-response.json
-                rm reindex-*
+                rm $BINARY_PREFIX-*
                 break;;
             No) echo "OK" ; break;;
         esac
